098454e1f99ec7e5daf43c1affcdb3f6391b50b0
957631042 <957631042@qq.com>
2020-05-08 21:55:10 +0800

    center修改

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -5,25 +5,18 @@
     <van-button type="info">信息按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
-    <van-index-bar>
-      <van-index-anchor index="A"/>
-      <van-cell title="文本" />
-      <van-cell title="文本" />
-      <van-cell title="文本" />
-
-      <van-index-anchor index="B"/>
-      <van-cell title="文本" />
-      <van-cell title="文本" />
-      <van-cell title="文本" />
-    </van-index-bar>
+    <div>122121212</div>
+    <div>122121212</div>
+    <div>122121212</div>
+    <div>122121212</div>
+    <div>122121212</div>
+    <div>122121212</div>
+    <div>122121212</div>
   </div>
 </template>
 <script>
 import vue from 'vue'
-import { Button, IndexBar, IndexAnchor } from 'vant'
+import { Button } from 'vant'
 vue.use(Button)
-vue.use(IndexBar)
-vue.use(IndexAnchor)
-
 export default {}
 </script>
